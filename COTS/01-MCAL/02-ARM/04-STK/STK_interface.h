8400268f6d5d6a8f16fbd781bb8d98e4ba409f4c
Alzahraa El-Sallakh <zahraaelsallakh@gmail.com>
2020-04-27 05:31:23 +0200

    Adding Delay function

--- COTS/01-MCAL/02-ARM/04-STK/STK_interface.h
+++ COTS/01-MCAL/02-ARM/04-STK/STK_interface.h
@@ -14,5 +14,6 @@
 void STK_voidInitialize (void);
 void STK_voidStart (u32 Copy_u32Value);
 void STK_voidSetCallBack(void(*Copy_Ptr)(void));
+void STK_voidBusyDelay(u32 Copy_u32Microseconds);
 
 #endif
--- COTS/01-MCAL/02-ARM/04-STK/STK_program.c
+++ COTS/01-MCAL/02-ARM/04-STK/STK_program.c
@@ -38,6 +38,23 @@ void STK_voidStart (u32 Copy_u32Value)
   SET_BIT(STK_CTRL,0);
 }
 
+void STK_voidBusyDelay(u32 Copy_u32Microseconds)
+{
+	/* Disable interrupt */
+	CLR_BIT(STK_CTRL,1);
+
+	/* Load "Load register" and clear "Value" */
+	STK_LOAD = Copy_u32Microseconds;
+	STK_VAL = 0;
+
+	/* Enable STK */
+	SET_BIT(STK_CTRL,0);
+
+	/* Wait flag */
+	while (GET_BIT(STK_CTRL,16) == 0);
+
+}
+
 void STK_voidSetCallBack(void(*Copy_Ptr)(void))
 {
     SysTick_CallBack = Copy_Ptr;
